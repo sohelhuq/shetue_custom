445b9b19a7c7513b44083a599d33335841bf23fc
sohelhuq <127501304+sohelhuq@users.noreply.github.com>
2025-11-30 18:04:10 +0600

    Create setup.py for Shetue custom package
    Initial setup configuration for Shetue custom package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shetue_custom',
+    version='0.0.1',
+    description='Customizations for Shetue MultiBiz ERPNext',
+    author='Sohel Huq',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
