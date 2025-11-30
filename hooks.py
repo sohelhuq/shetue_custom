0d9685aa94c0ffa1a91126f82ae68eca851e6427
sohelhuq <127501304+sohelhuq@users.noreply.github.com>
2025-11-30 22:58:14 +0600

    Delete hooks.py

--- hooks.py
+++
@@ -1,7 +0,0 @@
-app_name = "shetue_custom"
-app_title = "Shetue MultiBiz Custom App"
-app_publisher = "Sohel Huq"
-app_description = "Customizations for Shetue MultiBiz ERPNext"
-app_version = "0.0.1"
-# app_email = "your_email@example.com"
-# app_license = "MIT"
