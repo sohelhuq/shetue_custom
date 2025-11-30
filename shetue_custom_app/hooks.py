4e1498e54cdd7cd6b0351d23ee167ad2b20864fb
sohelhuq <127501304+sohelhuq@users.noreply.github.com>
2025-11-30 23:20:57 +0600

    Initialize hooks.py with app metadata

---
+++ shetue_custom_app/hooks.py
@@ -0,0 +1,7 @@
+app_name = "shetue_custom_app" # <--- পরিবর্তন করুন
+app_title = "Shetue MultiBiz Custom App"
+app_publisher = "Sohel Huq"
+app_description = "Customizations for Shetue MultiBiz ERPNext"
+app_version = "0.0.1"
+# app_email = "your_email@example.com"
+# app_license = "MIT"
